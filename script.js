35c5a6a4d55de5f36db600e4c7d71b032e9cb9b5
Dirty Soap <ytr67c@gmail.com>
2022-05-27 22:15:28 +0300

    add second cection

--- script.js
+++ script.js
@@ -4,3 +4,27 @@ burger.addEventListener('click', function(){
     burger.classList.toggle('active');
     burgerMenu.classList.toggle('active');
 })
+
+
+
+//swiper
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
+      },
+    },
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
